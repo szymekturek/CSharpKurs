ec7abe2707701bf90d43daf68a9ca056df312ead
szymekturek <szym.turek@gmail.com>
2019-02-12 15:44:45 +0100

    after changes

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,15 +10,70 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            ValueTypes();
 
-            Console.WriteLine("Hello" + name);
+            //string name = args[0];
+            //Console.WriteLine("Hello" + name);
 
             //foreach (var i in args)
             //{
-            //    Console.WriteLine("Hello," + i):
+            //    Console.WriteLine("Hello," + i);
             //}
-            Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Greetings();
+                Calculate();
+                Cleaning();
+            }
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("MaxInt = " + maxInt);
+            Console.WriteLine("MinInt = " + minInt);
+            Console.WriteLine("MaxLong = " + maxLong);
+            Console.WriteLine("MinLong = " + minLong);
+        }
+
+        private static void Cleaning()
+        {
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void Calculate()
+        {
+            Console.Write("Podaj liczbę: ");
+            int number;
+            bool result = int.TryParse(Console.ReadLine(), out number);
+            //number
+
+            //Console.WriteLine("Coś: " + name + ", Liczba: " + number);
+
+            if (number > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Number is positive");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("To nie jest liczba!");
+            }
+            else Console.WriteLine("Number is negative");
+        }
+
+        private static void Greetings()
+        {
+            Console.Write("Wpisz coś: ");
+            //string name;
+            string name = Console.ReadLine();
+            Console.WriteLine("Coś: " + name);
         }
     }
 }
